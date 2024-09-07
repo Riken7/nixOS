218f233dfc5b34c8cc47e529a80a1fde98a839eb
Riken7 <rikenprajapati2005@gmail.com>
2024-09-07 23:59:17 +0530

    zsh

--- home.nix
+++ home.nix
@@ -13,4 +13,25 @@
 
 	programs.home-manager.enable = true;
 	programs.neovim.enable = true;
+  programs.zsh = {
+      enable = true;
+      enableCompletion = true;
+      autosuggestion.enable = true;
+      syntaxHighlighting.enable = true;
+      
+      shellAliases = {
+        ll = "ls -l";
+        update = "sudo nixos-rebuild switch --flake .";
+        open = "xdg-open";
+      };
+      history = {
+        size = 10000;
+        path = "${config.xdg.dataHome}/zsh/history";
+      };
+      oh-my-zsh = {
+        enable = true;
+        plugins = [ "git" "direnv"];
+        theme = "robbyrussell";
+      };
+  };
 }
